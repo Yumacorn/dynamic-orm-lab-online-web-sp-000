e2ebf96a335fcab456628e0f6f4a98f527c68757
Ryan Mui <reachryanmui@gmail.com>
2020-03-15 23:56:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,13 +6,5 @@ require 'pry'
 class Student < InteractiveRecord
   attr_accessor :id, :name, :grade
 
-  def self.table_name
-    # self.class.table_name
-    self.to_s.downcase.pluralize
-  end
 
-  def self.column_names
-
-    # receives a Student return [id, name, grade]
-  end
 end
